6be144d165111d03c911e405e76dff29e89fab1a
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-05-20 19:38:19 -0300

    Update --> i18n
    - Added french translation.

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     /* BEGIN - TRANSLATIONS */
     const chooseLanguage = document.getElementById('id_change_language')
+    const defaultLanguage = 'en'
     
-    chooseLanguage.addEventListener('change', changeLanguage)
+    changeLanguage(defaultLanguage)
 
-    function changeLanguage() {
-        currentLanguage = chooseLanguage.value
-        path = 'i18n/' + currentLanguage + '.json'
+    chooseLanguage.addEventListener('change', () => {
+        const selectedLanguage = chooseLanguage.value
+        changeLanguage(selectedLanguage)
+    })
 
+    function changeLanguage(language) {
+        const path = `i18n/${language}.json`
+        
         fetch(path)
-        .then((response) => response.json())
-        .then((translations) => {
-            const elements = document.querySelectorAll('[data-translate]')
-            elements.forEach((element) => {
-            const key = element.getAttribute('data-translate')
-            element.textContent = translations[key]
+            .then((response) => response.json())
+            .then((translations) => {
+                const elements = document.querySelectorAll('[data-translate]')
+                elements.forEach((element) => {
+                    const key = element.getAttribute('data-translate')
+                    element.textContent = translations[key]
+                })
             })
-        })
     }
     /* END - TRANSLATIONS */
-
 })
